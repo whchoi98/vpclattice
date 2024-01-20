52e2f253f2c1eb077a4f9780009fe2603a97356f
whchoi98 <whchoi98@gmail.com>
2024-01-20 16:08:28 +0900

    edit - init.sh

--- cloud9/init.sh
+++ cloud9/init.sh
@@ -25,12 +25,30 @@ sudo mv ./yq_linux_amd64 /usr/local/bin/yq
 yq --version
 
 echo "------------------------------------------------------"
+cd ~
+curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.25.12/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+source <(kubectl completion bash)
+echo "source <(kubectl completion bash)" >> ~/.bashrc
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+kubectl version --short --client
+source <(kubectl completion bash)
+echo "source <(kubectl completion bash)" >> ~/.bashrc
 
-for command in jq envsubst aws
+sudo yum -y install jq gettext bash-completion moreutils
+for command in kubectl jq envsubst aws
   do
     which $command &>/dev/null && echo "$command in path" || echo "$command NOT FOUND"
   done
+ 
+  echo 'yq() {
+  docker run --rm -i -v "${PWD}":/workdir mikefarah/yq "$@"
+}' | tee -a ~/.bashrc && source ~/.bashrc
 
+K9S_VERSION=$(curl -s https://api.github.com/repos/derailed/k9s/releases/latest | jq -r '.tag_name')
+curl -sL https://github.com/derailed/k9s/releases/download/${K9S_VERSION}/k9s_Linux_amd64.tar.gz | sudo tar xfz - -C /usr/local/bin k9s
 
 curl -Lo ec2-instance-selector https://github.com/aws/amazon-ec2-instance-selector/releases/download/v2.4.1/ec2-instance-selector-linux-amd64
 chmod +x ec2-instance-selector
@@ -50,5 +68,11 @@ echo export ACCOUNT_ID=$(aws sts get-caller-identity --output text --query Accou
 #export CLOUD9_ID=($(aws cloud9 describe-environments --environment-ids ${C9_IDS} | jq -r '.environments[] | select(.name == "LatticeCloud9") | .id'))
 
 #echo export CLOUD9_ID=${C9_IDS} >> ~/env.sh
+echo "Session manager install"
+
+echo "------------------------------------------------------"
+curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/linux_64bit/session-manager-plugin.rpm" -o "session-manager-plugin.rpm"
+sudo yum install -y session-manager-plugin.rpm
+
 
 log_text "Success" "Installed of CLI tools successful"
