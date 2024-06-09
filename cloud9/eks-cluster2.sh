2e78281077173b96f69ac937769b5d63e2f146d7
Woo Hyung Choi <whchoi98@gmail.com>
2024-06-09 16:15:29 +0900

    Update eks-cluster2.sh

--- cloud9/eks-cluster2.sh
+++ cloud9/eks-cluster2.sh
@@ -1,6 +1,45 @@
 #!/bin/bash
 
 #source ./helper.sh
+
+AZS=($(aws ec2 describe-availability-zones --query 'AvailabilityZones[].ZoneName' --output text --region "$AWS_REGION"))
+echo "export CLUSTER_VPC_ID=$(aws ec2 describe-vpcs --filters Name=tag:Name,Values=$VPC_NAME | jq -r '.Vpcs[].VpcId')" | tee -a ~/.bash_profile
+
+echo "export RATES_PublicSubnet01=$(aws ec2 describe-subnets --filters 'Name=tag:Name,Values=LatticeWorkshop-Rates-PublicSubnet01' | jq -r '.Subnets[].SubnetId')" | tee -a ~/.bash_profile
+echo "export RATES_PublicSubnet02=$(aws ec2 describe-subnets --filters 'Name=tag:Name,Values=LatticeWorkshop-Rates-PublicSubnet02' | jq -r '.Subnets[].SubnetId')" | tee -a ~/.bash_profile
+echo "export RATES_PublicSubnet03=$(aws ec2 describe-subnets --filters 'Name=tag:Name,Values=LatticeWorkshop-Rates-PublicSubnet03' | jq -r '.Subnets[].SubnetId')" | tee -a ~/.bash_profile
+echo "export RATES_PrivateSubnet01=$(aws ec2 describe-subnets --filters 'Name=tag:Name,Values=LatticeWorkshop-Rates-PrivateSubnet01' | jq -r '.Subnets[].SubnetId')" | tee -a ~/.bash_profile
+echo "export RATES_PrivateSubnet02=$(aws ec2 describe-subnets --filters 'Name=tag:Name,Values=LatticeWorkshop-Rates-PrivateSubnet02' | jq -r '.Subnets[].SubnetId')" | tee -a ~/.bash_profile
+echo "export RATES_PrivateSubnet03=$(aws ec2 describe-subnets --filters 'Name=tag:Name,Values=LatticeWorkshop-Rates-PrivateSubnet03' | jq -r '.Subnets[].SubnetId')" | tee -a ~/.bash_profile
+
+export public_mgmd_node="managed-frontend-workloads"
+export private_mgmd_node="managed-backend-workloads"
+echo "export public_mgmd_node=${public_mgmd_node}" | tee -a ~/.bash_profile
+echo "export private_mgmd_node=${private_mgmd_node}" | tee -a ~/.bash_profile
+
+#export PUBLIC_SUBNETS_LIST=($(aws ec2 describe-subnets --filters Name=vpc-id,Values=$CLUSTER_VPC_ID --query 'Subnets[?MapPublicIpOnLaunch==`false`].{AZ: AvailabilityZone, SUBNET: SubnetId}' --output json))
+#export PRIVATE_SUBNETS_LIST=($(aws ec2 describe-subnets --filters Name=vpc-id,Values=$CLUSTER_VPC_ID --query 'Subnets[?MapPublicIpOnLaunch==`true`].{AZ: AvailabilityZone, SUBNET: SubnetId}' --output json))
+#echo "Identified Public subnets ${PUBLIC_SUBNETS_LIST[@]}"
+#echo "Identified Private subnets ${PRIVATE_SUBNETS_LIST[@]}"
+#AZ1=${AZS[0]}
+#AZ2=${AZS[1]}
+#AZ3=${AZS[2]}
+#PUBLIC_SUBNETS[0]=$(echo ${PUBLIC_SUBNETS_LIST[@]} | jq -r --arg AZ "$AZ1" '.[] | select(.AZ == $AZ ).SUBNET')
+#PUBLIC_SUBNETS[1]=$(echo ${PUBLIC_SUBNETS_LIST[@]} | jq -r --arg AZ "$AZ2" '.[] | select(.AZ == $AZ ).SUBNET')
+#PUBLIC_SUBNETS[2]=$(echo ${PUBLIC_SUBNETS_LIST[@]} | jq -r --arg AZ "$AZ3" '.[] | select(.AZ == $AZ ).SUBNET')
+#PRIVATE_SUBNETS[0]=$(echo ${PRIVATE_SUBNETS_LIST[@]} | jq -r --arg AZ "$AZ1" '.[] | select(.AZ == $AZ ).SUBNET')
+#PRIVATE_SUBNETS[1]=$(echo ${PRIVATE_SUBNETS_LIST[@]} | jq -r --arg AZ "$AZ2" '.[] | select(.AZ == $AZ ).SUBNET')
+#PRIVATE_SUBNETS[2]=$(echo ${PRIVATE_SUBNETS_LIST[@]} | jq -r --arg AZ "$AZ3" '.[] | select(.AZ == $AZ ).SUBNET')
+#echo ${PUBLIC_SUBNETS[*]}
+#echo ${PRIVATE_SUBNETS[*]}
+#aws ec2 create-tags --resources ${PUBLIC_SUBNETS[0]} --tags Key=kubernetes.io/cluster/${CLUSTER2_NAME},Value=shared Key=kubernetes.io/role/elb,Value=1 Key=alpha.eksctl.io/cluster-name,Value=${CLUSTER2_NAME}
+#aws ec2 create-tags --resources ${PUBLIC_SUBNETS[1]} --tags Key=kubernetes.io/cluster/${CLUSTER2_NAME},Value=shared Key=kubernetes.io/role/elb,Value=1 Key=alpha.eksctl.io/cluster-name,Value=${CLUSTER2_NAME}
+#aws ec2 create-tags --resources ${PUBLIC_SUBNETS[2]} --tags Key=kubernetes.io/cluster/${CLUSTER2_NAME},Value=shared Key=kubernetes.io/role/elb,Value=1 Key=alpha.eksctl.io/cluster-name,Value=${CLUSTER2_NAME}
+#aws ec2 create-tags --resources ${PRIVATE_SUBNETS[0]} --tags Key=kubernetes.io/cluster/${CLUSTER2_NAME},Value=shared Key=kubernetes.io/role/internal-elb,Value=1 Key=alpha.eksctl.io/cluster-name,Value=${CLUSTER2_NAME}
+#aws ec2 create-tags --resources ${PRIVATE_SUBNETS[1]} --tags Key=kubernetes.io/cluster/${CLUSTER2_NAME},Value=shared Key=kubernetes.io/role/internal-elb,Value=1 Key=alpha.eksctl.io/cluster-name,Value=${CLUSTER2_NAME}
+#aws ec2 create-tags --resources ${PRIVATE_SUBNETS[2]} --tags Key=kubernetes.io/cluster/${CLUSTER2_NAME},Value=shared Key=kubernetes.io/role/internal-elb,Value=1 Key=alpha.eksctl.io/cluster-name,Value=${CLUSTER2_NAME}
+#echo "Completed adding EKS tags to be make subnets compliant"
+
 source ~/.bash_profile
 
 cat << EOF > ~/environment/vpclattice/cloud9/lattice_eks02.yaml
@@ -19,23 +58,23 @@ vpc:
     public:
       PublicSubnet01:
         az: ${AWS_REGION}a
-        id: ${CLIENTS_PublicSubnet01}
+        id: ${RATES_PublicSubnet01}
       PublicSubnet02:
         az: ${AWS_REGION}b
-        id: ${CLIENTS_PublicSubnet02}
+        id: ${RATES_PublicSubnet02}
       PublicSubnet03:
         az: ${AWS_REGION}c
-        id: ${CLIENTS_PublicSubnet03}
+        id: ${RATES_PublicSubnet03}
     private:
       PrivateSubnet01:
         az: ${AWS_REGION}a
-        id: ${CLIENTS_PrivateSubnet01}
+        id: ${RATES_PrivateSubnet01}
       PrivateSubnet02:
         az: ${AWS_REGION}b
-        id: ${CLIENTS_PrivateSubnet02}
+        id: ${RATES_PrivateSubnet02}
       PrivateSubnet03:
         az: ${AWS_REGION}c
-        id: ${CLIENTS_PrivateSubnet03}
+        id: ${RATES_PrivateSubnet03}
 
 cloudWatch:
     clusterLogging:
@@ -61,9 +100,9 @@ managedNodeGroups:
   desiredCapacity: 3
   instanceType: ${NODE_INSTANCE_TYPE}
   subnets:
-    - ${CLIENTS_PrivateSubnet01}
-    - ${CLIENTS_PrivateSubnet02}
-    - ${CLIENTS_PrivateSubnet03}
+    - ${RATES_PrivateSubnet01}
+    - ${RATES_PrivateSubnet02}
+    - ${RATES_PrivateSubnet03}
   volumeSize: 20
   volumeType: gp3
   privateNetworking: true
@@ -84,6 +123,5 @@ managedNodeGroups:
 
 EOF
 
-
 cd ~/environment/vpclattice/cloud9/
-eksctl create cluster -f lattice_eks01.yaml --dry-run
+eksctl create cluster -f lattice_eks02.yaml --dry-run
